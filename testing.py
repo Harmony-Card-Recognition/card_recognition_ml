4279a5f16ad69f8f855c49e41b4043aaebe262d8
Jude Shin <judes946@gmail.com>
2024-05-13 22:07:41 -0700

    cleaned up random files

--- testing.py
+++ testing.py
@@ -2,5 +2,5 @@ from json_processing import create_smaller_json, filter_attributes_json
 
 
 
-filter_attributes_json(create_smaller_json('./.data/deckdrafterprod.MTGCard.json', 10))
+# filter_attributes_json(create_smaller_json('./.data/deckdrafterprod.MTGCard.json', 10))
 

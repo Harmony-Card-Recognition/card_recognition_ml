de85396c83012d972cad9109a3f264cca190c6e2
Jude Shin <judes946@gmail.com>
2024-07-19 10:33:46 -0700

    changed negative_slope to alpha

--- models/cnn_model/cnn.py
+++ models/cnn_model/cnn.py
@@ -89,28 +89,28 @@ def train_model(
     model = models.Sequential()
     model.add(layers.InputLayer(input_shape=(img_width, img_height, 3)))
     model.add(layers.Conv2D(64, (3, 3)))
-    model.add(layers.LeakyReLU(negative_slope=0.01))
+    model.add(layers.LeakyReLU(alpha=0.01))
     model.add(layers.MaxPooling2D(2, 2))
 
     model.add(layers.Conv2D(128, (3, 3)))
-    model.add(layers.LeakyReLU(negative_slope=0.01))
+    model.add(layers.LeakyReLU(alpha=0.01))
     model.add(layers.MaxPooling2D(2, 2))
 
     model.add(layers.Conv2D(256, (3, 3)))
-    model.add(layers.LeakyReLU(negative_slope=0.01))
+    model.add(layers.LeakyReLU(alpha=0.01))
     model.add(layers.MaxPooling2D(2, 2))
 
     model.add(layers.Conv2D(256, (3, 3)))
-    model.add(layers.LeakyReLU(negative_slope=0.01))
+    model.add(layers.LeakyReLU(alpha=0.01))
     model.add(layers.MaxPooling2D(2, 2))
 
     model.add(layers.Conv2D(512, (3, 3)))
-    model.add(layers.LeakyReLU(negative_slope=0.01))
+    model.add(layers.LeakyReLU(alpha=0.01))
     model.add(layers.MaxPooling2D(2, 2))
 
     model.add(layers.Flatten())
     model.add(layers.Dense(2048))
-    model.add(layers.LeakyReLU(negative_slope=0.01))
+    model.add(layers.LeakyReLU(alpha=0.01))
     model.add(layers.Dropout(0.5))
     model.add(layers.Dense(unique_classes, activation='softmax'))
 

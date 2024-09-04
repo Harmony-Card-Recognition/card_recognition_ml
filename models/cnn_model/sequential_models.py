37a3777a8e4620e2a3923bf284af1dd6fbe8bdb1
Jude Shin <judes946@gmail.com>
2024-09-04 09:30:27 -0700

    less regularization

--- models/cnn_model/sequential_models.py
+++ models/cnn_model/sequential_models.py
@@ -210,35 +210,35 @@ def model_8(img_width, img_height, unique_classes):
     model.add(layers.InputLayer(shape=(img_width, img_height, 3)))
 
     # First Convolutional Block
-    model.add(layers.Conv2D(64, (3, 3), kernel_regularizer=regularizers.l2(0.01)))  # Added L2 regularization
+    model.add(layers.Conv2D(64, (3, 3), kernel_regularizer=regularizers.l2(0.001)))  # Added L2 regularization
     model.add(layers.LeakyReLU(negative_slope=0.01))
     model.add(layers.MaxPooling2D(2, 2))
 
     # Second Convolutional Block
-    model.add(layers.Conv2D(128, (3, 3), kernel_regularizer=regularizers.l2(0.01)))  # Added L2 regularization
+    model.add(layers.Conv2D(128, (3, 3), kernel_regularizer=regularizers.l2(0.001)))  # Added L2 regularization
     model.add(layers.LeakyReLU(negative_slope=0.01))
     model.add(layers.MaxPooling2D(2, 2))
 
     # Third Convolutional Block
-    model.add(layers.Conv2D(256, (3, 3), kernel_regularizer=regularizers.l2(0.01)))  # Added L2 regularization
+    model.add(layers.Conv2D(256, (3, 3), kernel_regularizer=regularizers.l2(0.001)))  # Added L2 regularization
     model.add(layers.LeakyReLU(negative_slope=0.01))
     model.add(layers.MaxPooling2D(2, 2))
 
     # Fourth Convolutional Block
-    model.add(layers.Conv2D(256, (3, 3), kernel_regularizer=regularizers.l2(0.01)))  # Added L2 regularization
+    model.add(layers.Conv2D(256, (3, 3), kernel_regularizer=regularizers.l2(0.001)))  # Added L2 regularization
     model.add(layers.LeakyReLU(negative_slope=0.01))
     model.add(layers.MaxPooling2D(2, 2))
 
     # Fifth Convolutional Block
-    model.add(layers.Conv2D(512, (3, 3), kernel_regularizer=regularizers.l2(0.01)))  # Added L2 regularization
+    model.add(layers.Conv2D(512, (3, 3), kernel_regularizer=regularizers.l2(0.001)))  # Added L2 regularization
     model.add(layers.LeakyReLU(negative_slope=0.01))
     model.add(layers.MaxPooling2D(2, 2))
 
     # Flatten and Fully Connected Layers
     model.add(layers.Flatten())
-    model.add(layers.Dense(2048, kernel_regularizer=regularizers.l2(0.01)))  # Added L2 regularization
+    model.add(layers.Dense(2048, kernel_regularizer=regularizers.l2(0.001)))  # Added L2 regularization
     model.add(layers.LeakyReLU(negative_slope=0.01))
-    model.add(layers.Dropout(0.6))  # Increased dropout rate from 0.5 to 0.6
+    model.add(layers.Dropout(0.5))  # Increased dropout rate from 0.5 to 0.6
     model.add(layers.Dense(unique_classes, activation='softmax'))
 
     return model
